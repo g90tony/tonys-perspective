0e0cd329924774b7d0c0e58bf393591c4bd07970
Caleb Tony <calebmbugua@gmail.com>
2021-05-02 13:14:52 +0300

    added quote of the day request

--- app/request.py
+++ app/request.py
@@ -7,4 +7,25 @@ def configure_request(app):
     quotes_url = app.config['QUOTES_API_URL']
     pexels_api_key = app.config['PEXELS_API_KEY']
     pexels_url = app.config['PEXELS_URL']
-'
+
+
+def get_quote_of_the_day():
+        
+    with urllib.request.urlopen(quotes_url) as url:
+        
+        api_response = url.read()
+        
+        quote_json = json.loads(api_response)
+        
+        quote_data = quote_json.content.quotes[0]
+        
+        quoteOBJ = dict()
+        
+        quoteOBJ['quote']  = quote_data.quote
+        quoteOBJ['author'] =quotes_data.author
+        quoteOBJ['image_url'] = quote_data.background
+        
+        return quoteOBJ
+        
+    
+
