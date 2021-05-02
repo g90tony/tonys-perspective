297860679675c0c84ea9ca1b535e92f22d7ff754
Caleb Tony <calebmbugua@gmail.com>
2021-05-02 14:26:14 +0300

    added a comment submit form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import SubmitField, TextAreaField
 
 class LoadMoreArticles(FlaskForm):
     loadMore = SubmitField('Load More')
+    
+    
+class AddComment(FlaskForm):
+    comment_input = TextAreaField(validators=[Required()])
+    submit = SubmitField('Post')
     
