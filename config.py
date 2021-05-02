10e7163a8c30583ddf4b25d0fffab558645a0fd2
Caleb Tony <calebmbugua@gmail.com>
2021-05-02 13:11:01 +0300

    added API keys and urls

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
 
 class Config:
+    
+    QUOTES_URL = os.environ.get('QUOTES_URL')
+    PEXELS_API_KEY = os.environ.get('PEXELS_API_KEY')
+    PEXELS_URL = os.environ.get('PEXELS_URL')
+    
     @staticmethod
     def init_app(app):
         pass
