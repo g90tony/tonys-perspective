f31d17da46415e48cb685e8e85502c5a57a8f584
Caleb Tony <calebmbugua@gmail.com>
2021-05-03 13:35:30 +0300

    fixed minor typo

--- app/request.py
+++ app/request.py
@@ -2,7 +2,7 @@ import urllib.request, json
 from .models import Article
 
 def configure_request(app):
-    globals quotes_url, pexels_api_key, pexels_url
+    global quotes_url, pexels_api_key, pexels_url
     
     quotes_url = app.config['QUOTES_API_URL']
     pexels_api_key = app.config['PEXELS_API_KEY']
