669024f2e44f9de623603abca465e4de8b7c9de2
Caleb Tony <calebmbugua@gmail.com>
2021-05-03 13:46:49 +0300

    created welcome email template

---
+++ app/email.py
@@ -0,0 +1,12 @@
+from flask_mail import Message
+from flask import render_template
+from . import mail
+
+def welcome_message(subject, template, to, **kwargs):
+    sender_email = ""
+    
+    email = Message(subject, sender = sender_email, recipients = [to])
+    email.body = render_template(template + '.txt', **kwargs)
+    email.body = render_template(template + '.html', **kwargs)
+    
+    mail.send(email)
