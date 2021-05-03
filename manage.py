123ffa083300184f2a1eb85d692a87dc615e4523
Caleb Tony <calebmbugua@gmail.com>
2021-05-03 13:12:40 +0300

    imported db from __init__.py
    added manager commans for migration and server initalization

--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
-from app import create_app
+from app import create_app, db
 
 app = create_app('development')
+
 manager = Manager(app)
-server = Server('server', Server)
+migrate= Migrate(app, db)
+
+
+manager.add_command('db', MigrateCommand)
+manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
