23912e9f5eb5bb01c78160609b9d04a309727659
Caleb Tony <calebmbugua@gmail.com>
2021-05-03 13:47:45 +0300

    imported and intanciated it

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
 
 from config import config_options
 
@@ -15,6 +16,8 @@ login_manager.login_view = 'auth.login'
 
 photos = UploadSet('photos', IMAGES)
 
+mail = Mail()
+
 
 def create_app(config_name):
      app = Flask(__name__)
@@ -24,7 +27,10 @@ def create_app(config_name):
      bootstrap.init_app(app)
      db.init_app(app)
      login_manager.init_app(app)
+     mail.init_app(app)
+     
      configure_uploads(app, photos)
+     
      
      from .main import main as main_blueprint
      app.register_blueprint(main_blueprint)
