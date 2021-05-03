07b6f9998e7919f69e152c044673579327dde7fc
Caleb Tony <calebmbugua@gmail.com>
2021-05-03 17:37:16 +0300

    added create article route handler

---
+++ app/admin/views.py
@@ -0,0 +1,38 @@
+from datetime import datetime, date
+
+from flask import render_template, url_for, flask, redirect
+from flask_log import login_required
+from . import admin
+from .forms import CreateNewArticle
+
+from .. import db
+from ..models import Article, Category
+from ..request import get_pexels_image
+
+
+@login_required
+@admin.route('/admin/create-article', methods['GET, POST'])
+def create_article():
+    title = "Tony's Perspective: Admin"
+    
+    form = CreateNewArticle()
+    
+    if form.validate_on_submit():
+        new_article_title = form.title.data
+        new_article_category = form.category.data
+        new_article_content = form.content.data
+        new_date = date.today()
+        
+        selected_category = Category.query.filter_by(id = new_article_category).first()
+        
+        image_url = get_pexels_image(selected_category.title)
+        
+        formated_date = new_date.strftime("%a, %d %B, %y")
+        
+        
+        new_article = Article(image_url=image_url ,date=formated_date ,category= new_article_category ,content= new_article_content,views= 0)
+        new_article.save_article()
+        
+        return redirect( url_for('admin.create_article'))
+    
+    return render_template('admin/create.html' form = form, title = title)
