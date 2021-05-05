69f65f312992a23b646f8b888babb4726e33ea05
Caleb Tony <calebmbugua@gmail.com>
2021-05-05 04:57:27 +0300

    updated label text

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,12 +6,12 @@ from ..models import User
 
 
 class RegistrationForm(FlaskForm):
-    first_nameLabel = Markup(" <p class='custom-bold-body mb-0'>Enter first name</p>")
-    last_nameLabel = Markup(" <p class='custom-bold-body mb-0'>Enter last name</p>")
-    emailLabel = Markup(" <p class='custom-bold-body mb-0'>Enter email address</p>")
-    avatarLabel = Markup(" <p class='custom-bold-body mb-0'>Upload profile picture</p>")
-    passwordLabel = Markup(" <p class='custom-bold-body mb-0'>Enter new password</p>")
-    password2Label = Markup(" <p class='custom-bold-body mb-0'>Confirm new password</p>")
+    first_nameLabel = Markup(" <p class='article-title mb-0'>Enter first name</p>")
+    last_nameLabel = Markup(" <p class='article-title mb-0'>Enter last name</p>")
+    emailLabel = Markup(" <p class='article-title mb-0'>Enter email address</p>")
+    avatarLabel = Markup(" <p class='article-title mb-0'>Upload profile picture</p>")
+    passwordLabel = Markup(" <p class='article-title mb-0'>Enter new password</p>")
+    password2Label = Markup(" <p class='article-title mb-0'>Confirm new password</p>")
     first_name = StringField(first_nameLabel, validators = [(Required())])
     last_name = StringField(last_nameLabel, validators = [Required()])
     email = StringField(emailLabel, validators = [Required(), Email()])
@@ -19,13 +19,13 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField(password2Label, validators = [Required()])
 
 class LoginForm(FlaskForm):
-    emailLabel = Markup(" <p class='custom-bold-body mb-0'>Enter email address</p>")
-    passwordLabel = Markup(" <p class='custom-bold-body mb-0'>Enter your password</p>")
+    emailLabel = Markup(" <p class='article-title mb-0'>Enter email address</p>")
+    passwordLabel = Markup(" <p class='article-title mb-0'>Enter your password</p>")
     email = StringField(emailLabel, validators = [Required(), Email()])
     password = PasswordField(passwordLabel, validators = [Required()])
     remember_user = BooleanField('Remember me')
 
 class AdminLoginForm(FlaskForm):
-    passwordLabel = Markup(" <p class='custom-bold-body mb-0'>Enter your password</p>")
+    passwordLabel = Markup(" <p class='article-title mb-0'>Enter your password</p>")
     password = PasswordField(passwordLabel, validators = [Required()])
     remember_user = BooleanField('Remember me')
